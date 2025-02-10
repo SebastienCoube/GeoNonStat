347149f40a3ca7578bb6404ee9264a41fd84ef41
Sebastien Coube <secoube@ledormeur.toulouse.inrae.fr>
2025-02-10 15:48:18 +0100

    "adding initialization tset"

--- Tset/Tset_initialisation.R
+++ Tset/Tset_initialisation.R
@@ -15,7 +15,7 @@ NNarray_aniso = GpGp::find_ordered_nn(locs[seq(10000),], 10)
 
 # getting the coefficients
 chol_precision = 
-  Bidart::compute_sparse_chol(
+  GeoNonStat::compute_sparse_chol(
     range_beta = range_beta, # range parameters
     NNarray = NNarray_aniso, # Vecchia approx DAG
     locs = locs[seq(10000),], # spatial coordinates
@@ -36,22 +36,11 @@ seed_vector = rnorm(10000)
 aniso_latent_field = as.vector(Matrix::solve(chol_precision, seed_vector)) 
 aniso_observed_field = aniso_latent_field + .8*rnorm(10000)
 
-Bidart::plot_pointillist_painting(locs, aniso_latent_field)
+GeoNonStat::plot_pointillist_painting(locs, aniso_latent_field)
   
-MCMC_NNGP = Bidart::initialize(
+MCMC_NNGP = GeoNonStat::initialize(
   observed_locs = locs[seq(10000),], observed_field = aniso_observed_field, 
   nu = 1.5, n_chains = 5,
   range_PP = T, PP = PP, # use PP for range
   anisotropic = T # Covariance will be anisotropic
 )
-
-list2env(mcmc_ngp_list_aniso, envir = environment())
-state = states[[1]]
-iter = 30
-iter_start = 0
-
-mcmc_ngp_list_aniso = Bidart::mcmc_nngp_run_nonstationary_socket(mcmc_ngp_list_aniso, num_threads_per_chain = 30, burn_in = .5, thinning = 1)
-
-Bidart::diagnostic_plots(mcmc_ngp_list_aniso, burn_in = .5, starting_proportion = .02)
-Bidart::diagnostic_plots(mcmc_ngp_list_aniso, burn_in = .5, starting_proportion = .2)
-Bidart::diagnostic_plots(mcmc_ngp_list_aniso, burn_in = .2, starting_proportion = .5)
