cdd5241383af9ffea5c43d389569e27cb7c08979
Elise Maigné <elise.maigne@inrae.fr>
2025-05-13 14:57:31 +0200

    Add test process_vecchia

--- tests/testthat/test-GeoNonStat.R
+++ tests/testthat/test-GeoNonStat.R
@@ -1,3 +1,43 @@
+test_that("process_vecchia GeoNonStat works", {
+  set.seed(100)
+  size <- 2000
+  observed_locs = cbind(runif(size), runif(size))
+  expect_message(
+  res <- process_vecchia(observed_locs, m=10),
+    "building DAGs and indices for Vecchia approximation"
+  )
+  expect_true("list" %in% class(res))
+  expect_named(res, c('n_locs', 'n_obs', 'locs', 'locs_match', 
+                      'locs_match_matrix', 'hctam_scol', 
+                      'hctam_scol_1', 'obs_per_loc', 
+                      'NNarray', 'NNarray_non_NA', 
+                      'sparse_chol_column_idx', 'sparse_chol_row_idx', 
+                      'locs_partition'))
+  expect_identical(res$n_locs, 2000L)
+  expect_identical(res$n_obs, 2000L)
+  expect_true(is.numeric(res$locs))
+  expect_identical(dim(res$locs), c(2000L, 2L))
+  expect_equal(mean(res$locs), 0.5035152, tolerance = 1e-5)
+  expect_identical(length(res$locs_match), 2000L)
+  expect_true(all(res$locs_match %in% seq_len(res$n_locs)))
+  
+  expect_true("Matrix" %in% class(res$locs_match_matrix))
+  expect_identical(dim(res$locs_match_matrix), c(2000L, 2000L))
+  
+  expect_true("list" %in% class(res$hctam_scol))
+  expect_true(all(res$hctam_scol %in% seq_len(res$n_locs)))
+  expect_true(all(res$hctam_scol_1 %in% seq_len(res$n_locs)))
+  
+  expect_true(all(unlist(res$hctam_scol) == res$hctam_scol_1))
+  expect_true(all(res$obs_per_loc == 1))
+  
+  expect_identical(dim(res$NNarray), c(11L, 2000L))
+  expect_identical(length(res$sparse_chol_column_idx), 21945L)
+  expect_identical(length(res$sparse_chol_row_idx), 21945L)
+  expect_identical(dim(res$locs_partition), c(2000L, 1L))
+})
+
+
 test_that("initialize class GeoNonStat works", {
   set.seed(100)
   size <- 2000
