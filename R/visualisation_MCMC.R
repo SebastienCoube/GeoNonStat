9f6d1fdb82c1459ade5635578352809b5a5d403c
Elise Maigné <elise.maigne@inrae.fr>
2025-12-19 17:15:07 +0100

    TracePlots OK

--- R/visualisation_MCMC.R
+++ R/visualisation_MCMC.R
@@ -22,26 +22,80 @@ PrintMcmcDiags = function(object, burn_in = .1, min_ESS = 50, max_Gelman_upper =
   return(list("diags"=res, "which.worst"=which.worst))
 }
 
+TraceOneParam = function(name, param, jstart, jend, xlim, color_lines){
+  dimparam <- jend - jstart + 1
+  ncol <- min(dimparam, ifelse(dimparam>9, 4, 3))
+  nrow <- ceiling(dimparam / ncol)
+  par(mfrow = c(nrow, ncol))
+  
+  # Iterate over columns
+  for(col in seq(jstart, jend)) { 
+    namecol <- colnames(param[[1]])[col]
+    to_be_plotted <- do.call("cbind", lapply(param, function(x) x[,col]))
+    matplot(to_be_plotted, 
+            type="l", 
+            lty = 1,
+            lwd=2,
+            main  = paste0(name, "\n", namecol),
+            col = color_lines)
+  }
+}
+
+split_intervals <- function(total_size, starts, nmax =  16) {
+  intervals <- list()
+  ends <- c(starts[-1] - 1, total_size)
+  for (i in seq_along(starts)) {
+    s <- starts[i]
+    e <- ends[i]
+    
+    while ((e - s + 1) > nmax) {
+      intervals <- append(intervals, list(c(s, s + nmax - 1)))
+      s <- s + nmax
+    }
+    intervals <- append(intervals, list(c(s, e)))
+  }
+  intervals
+}
+
 TracePlots = function(object, burn_in = .1, keep = "all", mfrow = c(3,2)){
-  par(mfrow = mfrow)
+  ormfrow <- par("mfrow")
+  omar <- par("mar")
+  par(mar=c(2.1, 3.1, 3.1, 1.1))
   n_iters <- length(object$records[[1]])
   records = aggregateRecords(object, burn_in, keep = keep, keep_separate_chains = TRUE)
   records <- transposeList(records)
+  names_params <- names(records)
+  n_chain <- length(records[[1]])
   plotted_iters = seq(max(0, floor(n_iters * burn_in))+1,n_iters)
+  
   xlim <- c(min(plotted_iters), max(plotted_iters))
-  nbchains <- length()
-  for(c in seq_len(ncol(records))){
-     for(name in names(records)[[1]]){
-       to_be_plotted
-      to_be_plotted = records[[c]][[name]]
-      plot(0, 0, 
-           ylim = c(min(to_be_plotted), max(to_be_plotted)), 
-           xlim = xlim, 
-           type = "n", 
-           main  = paste("trace plot of", name, colnames(records[[name]])))
-      for(j in seq_len(ncol(to_be_plotted))){
-        lines(plotted_iters, to_be_plotted[,j])
-      }
+  color_lines <- getColorsCat(3)
+  
+  for(name in names_params){
+    print(name)
+    if(name=="noise_beta"){
+      print("toto")
+    }
+    refparams <- records[[name]][[1]]
+    n <- dim(refparams)[2]
+    # For parms with several intercepts, split representation !
+    intercepts <- grep("Intercept", colnames(refparams))
+    columns <- list()
+    
+    if(length(intercepts)>0) {
+      starts <- intercepts
+    } else {
+      starts <- 1
+    }
+    columns <- split_intervals(n, starts, nmax = 16)
+   
+    for(i in length(columns)) {
+      TraceOneParam(name, records[[name]], columns[[i]][1], columns[[i]][2], xlim = xlim, color_lines = color_lines)
     }
   }
+    
+  # Resetting original parameters mfrow
+  par("mfrow"= ormfrow)
+  par("mar" = omar)
+  return(invisible(NULL))
 }
