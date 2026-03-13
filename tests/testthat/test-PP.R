f0367b1ee4d41cd517bd5e495e6e81bc738d0e62
Elise Maigné <elise.maigne@inrae.fr>
2026-03-13 13:43:45 +0100

    arguments in tests (probably not functional)

--- tests/testthat/test-PP.R
+++ tests/testthat/test-PP.R
@@ -1,29 +1,32 @@
 set.seed(123)
-vecchia_approx = createVecchia(cbind(runif(1000), runif(1000)), 10, ncores = 1)
+vecchia_approx <- createVecchia(cbind(runif(1000), runif(1000)), 10)
 
 test_that("createPP works (automatic)", {
   set.seed(123)
   expect_message(
-    pepito <- createPP(vecchia_approx, plot=FALSE), 
-    "number of knots set to 25")
+    pepito <- createPP(vecchia_approx, plot = FALSE),
+    "number of knots set to 25"
+  )
   expect_s3_class(pepito, "PP")
-  expect_named(pepito, c("knots", "matern_range", "sparse_chol","n_knots", "vecchia_locs"))
-  
+  expect_named(pepito, c("knots", "matern_range", "sparse_chol", "n_knots", "vecchia_locs"))
+
   # Format
-  expect_identical(sapply(pepito, function(x) class(x)[1]),
-                   c(
-                     "knots" = "matrix",
-                     "matern_range" = "numeric",
-                     "sparse_chol" = "dtCMatrix",
-                     "n_knots" = "integer",
-                     "vecchia_locs" = "matrix")
-                   )
+  expect_identical(
+    sapply(pepito, function(x) class(x)[1]),
+    c(
+      "knots" = "matrix",
+      "matern_range" = "numeric",
+      "sparse_chol" = "dtCMatrix",
+      "n_knots" = "integer",
+      "vecchia_locs" = "matrix"
+    )
+  )
   expect_identical(dim(pepito$knots), c(25L, 2L))
   expect_identical(dim(pepito$sparse_chol), c(1025L, 1025L))
   expect_identical(dim(pepito$vecchia_locs), c(1000L, 2L))
-  
+
   # values
-  expect_identical(rownames(pepito$knots)[1:5], c("21", "5",  "8",  "18", "25"))
+  expect_identical(rownames(pepito$knots)[1:5], c("21", "5", "8", "18", "25"))
   # expect_equal(colMeans(pepito$knots), c(0.4506183, 0.4971161), tolerance = 1e-5)
   # expect_equal(sum(pepito$sparse_chol[1:10,1:10]), 4.105683, tolerance = 1e-5)
   # expect_equal(pepito$matern_range, 0.2906949, tolerance = 1e-5)
@@ -36,13 +39,13 @@ test_that("createPP works for all parameters", {
   expect_s3_class(pp1, "PP")
   expect_true(is.matrix(pp1$knots))
   expect_true(pp1$matern_range > 0)
-  
+
   pp2 <- createPP(vecchia_approx, matern_range = 0.1, plot = FALSE)
   expect_equal(pp2$matern_range, 0.1)
-  
+
   pp3 <- createPP(vecchia_approx, knots = 50, plot = FALSE)
   expect_equal(pp3$n_knots, 50)
-  
+
   grid_knots <- as.matrix(expand.grid(seq(0, 1, 0.2), seq(0, 1, 0.2)))
   pp4 <- createPP(vecchia_approx, knots = grid_knots, plot = FALSE)
   expect_equal(nrow(pp4$knots), nrow(grid_knots))
@@ -51,28 +54,30 @@ test_that("createPP works for all parameters", {
 test_that("createPP works matern_range only", {
   set.seed(123)
   expect_message(
-    pepito <- createPP(vecchia_approx, matern_range = .1, plot=FALSE), 
-    "number of knots set to 25")
+    pepito <- createPP(vecchia_approx, matern_range = .1, plot = FALSE),
+    "number of knots set to 25"
+  )
   expect_s3_class(pepito, "PP")
   # values
-  expect_equal(sum(pepito$sparse_chol[1:10,1:10]), 7.677429, tolerance = 1e-5)
+  expect_equal(sum(pepito$sparse_chol[1:10, 1:10]), 7.677429, tolerance = 1e-5)
   expect_equal(pepito$matern_range, .1, tolerance = 1e-5)
 })
 
 test_that("createPP works matern_range and nb knots", {
   set.seed(123)
   expect_message(
-    pepito <- createPP(vecchia_approx, knots = 50, matern_range = .1, plot=FALSE), 
-    "knot placement done by default using k-means")
+    pepito <- createPP(vecchia_approx, knots = 50, matern_range = .1, plot = FALSE),
+    "knot placement done by default using k-means"
+  )
   expect_s3_class(pepito, "PP")
-  # Format 
+  # Format
   expect_identical(dim(pepito$knots), c(50L, 2L))
   expect_identical(dim(pepito$sparse_chol), c(1050L, 1050L))
-  
+
   # values
   expect_identical(rownames(pepito$knots)[1:5], c("9", "49", "10", "43", "13"))
   expect_equal(colMeans(pepito$knots), c(0.4499628, 0.4944043), tolerance = 1e-5)
-  expect_equal(sum(pepito$sparse_chol[1:10,1:10]), 6.894951, tolerance = 1e-5)
+  expect_equal(sum(pepito$sparse_chol[1:10, 1:10]), 6.894951, tolerance = 1e-5)
   expect_equal(pepito$matern_range, .1, tolerance = 1e-5)
 })
 
@@ -80,61 +85,67 @@ test_that("createPP works knots as matrix", {
   set.seed(123)
   myknots <- as.matrix(expand.grid(seq(-.05, 1.05, .05), seq(-.05, 1.05, .05)))
   expect_message(
-    pepito <- createPP(vecchia_approx, knots = myknots, matern_range = .1), 
-    "This is great")
+    pepito <- createPP(vecchia_approx, knots = myknots, matern_range = .1),
+    "This is great"
+  )
   expect_s3_class(pepito, "PP")
-  # Format 
+  # Format
   expect_identical(dim(pepito$knots), dim(myknots))
   expect_identical(dim(pepito$sparse_chol), c(1529L, 1529L))
-  
+
   # values
   expect_identical(rownames(pepito$knots)[1:5], c("383", "283", "412", "249", "300"))
   expect_equal(colMeans(pepito$knots), colMeans(myknots))
-  expect_equal(sum(pepito$sparse_chol[1:10,1:10]), 6.9976, tolerance = 1e-5)
+  expect_equal(sum(pepito$sparse_chol[1:10, 1:10]), 6.9976, tolerance = 1e-5)
   expect_equal(pepito$matern_range, .1, tolerance = 1e-5)
 })
 
 test_that("var_loss_percentage.PP gives expected results", {
   set.seed(123)
   suppressMessages({
-    pepito0 <- createPP(vecchia_approx, plot=FALSE)
-    pepito1 <- createPP(vecchia_approx, plot=FALSE, matern_range = 0.2)
-    pepito2 <- createPP(vecchia_approx, plot=FALSE, matern_range = 0.01)
+    pepito0 <- createPP(vecchia_approx, plot = FALSE)
+    pepito1 <- createPP(vecchia_approx, plot = FALSE, matern_range = 0.2)
+    pepito2 <- createPP(vecchia_approx, plot = FALSE, matern_range = 0.01)
   })
-  
+
   # Var loss ok
   expect_message(
-    res <- var_loss_percentage.PP(pepito0), 
-    "This is great")
+    res <- var_loss_percentage.PP(pepito0),
+    "This is great"
+  )
   expect_true(is(res, "numeric"))
   expect_equal(mean(res), 1.932659, tolerance = 1e-5)
-  
+
   # Var loss almost ok
   expect_message(
-    res <- var_loss_percentage.PP(pepito1), 
-    "This is fairly good")
+    res <- var_loss_percentage.PP(pepito1),
+    "This is fairly good"
+  )
   expect_equal(mean(res), 4.838341, tolerance = 1e-5)
-  
+
   # Var loss bad
   expect_message(
-    res <- var_loss_percentage.PP(pepito2), 
-    "his is quite a bit of loss")
+    res <- var_loss_percentage.PP(pepito2),
+    "his is quite a bit of loss"
+  )
   expect_equal(mean(res), 95.59835, tolerance = 1e-5)
 })
 
 test_that("summary.PP gives expected results", {
   set.seed(123)
   suppressMessages(
-    pepito <- createPP(vecchia_approx, plot=FALSE)
+    pepito <- createPP(vecchia_approx, plot = FALSE)
   )
-  expect_output(summary(pepito),
-                 "Object of class 'PP' with 25 knots, based on 1000 locations, matérn range =")
+  expect_output(
+    summary(pepito),
+    "Object of class 'PP' with 25 knots, based on 1000 locations, matérn range ="
+  )
 })
 
 set.seed(123)
-PP = createPP(vecchia_approx, plot=FALSE)
-X = matrix(rnorm(500), 1000)
-Y = matrix(rnorm(30*nrow(X)), nrow(X))
+PP <- createPP(vecchia_approx, plot = FALSE)
+X <- matrix(rnorm(500), 1000)
+Y <- matrix(rnorm(30 * nrow(X)), nrow(X))
 
 test_that("xPPCrossprod Simple crossprod if no PP, vecchia unused", {
   expect_error(
@@ -143,14 +154,14 @@ test_that("xPPCrossprod Simple crossprod if no PP, vecchia unused", {
   )
   expect_true(is(res1, "matrix"))
   expect_identical(dim(res1), c(1L, 30L))
-  expect_identical(res1,crossprod(X, Y))
-  
-  # Simple corssprod, vecchia unused. 
+  expect_identical(res1, crossprod(X, Y))
+
+  # Simple corssprod, vecchia unused.
   expect_error(
     res2 <- xPPCrossprod(X = X, PP = NULL, Y = Y, vecchia_approx = NULL),
     NA
   )
-  expect_identical(res1,res2)
+  expect_identical(res1, res2)
 })
 
 test_that("xPPCrossprod with PP", {
@@ -160,10 +171,10 @@ test_that("xPPCrossprod with PP", {
   )
   expect_true(is(res2, "matrix"))
   expect_identical(dim(res2), c(101L, 30L))
-  
+
   # First line is crossprod
-  expect_identical(res2[1,],crossprod(X, Y)[1,])
-  # expect_equal(colMeans(res2)[1:5], 
+  expect_identical(res2[1, ], crossprod(X, Y)[1, ])
+  # expect_equal(colMeans(res2)[1:5],
   #              c(0.1999806, -0.8748530, 0.9684133, -0.7444476, 0.1350033),
   #              tolerance = 1e-5)
 })
@@ -175,10 +186,10 @@ test_that("xPPCrossprod with PP and permute obs", {
   )
   expect_true(is(res3, "matrix"))
   expect_identical(dim(res3), c(101L, 30L))
-  
+
   # First line is crossprod
-  expect_identical(res3[1,],crossprod(X, Y)[1,])
-  # expect_equal(colMeans(res3)[1:5], 
+  expect_identical(res3[1, ], crossprod(X, Y)[1, ])
+  # expect_equal(colMeans(res3)[1:5],
   #              c(0.02263887, -1.04065836, 0.84462225, -0.70220738, 0.36781311),
   #              tolerance = 1e-5)
 })
@@ -198,7 +209,7 @@ test_that("xPPMultRight with PP and permute obs", {
   expect_true(is(resmr, "matrix"))
   expect_identical(dim(resmr), c(1000L, 4L))
   # expect_equal(mean(resmr), 0.01392224, tolerance = 1e-5)
-  
+
   # No X
   expect_error(
     resmr <- xPPMultRight(X = NULL, PP = PP, Y = matrix(rnorm(200), nrow = 100), vecchia_approx = vecchia_approx, permutate_PP_to_obs = FALSE),
@@ -208,14 +219,13 @@ test_that("xPPMultRight with PP and permute obs", {
   expect_identical(dim(resmr), c(1000L, 2L))
   mean(as.matrix(resmr))
   # expect_equal(mean(as.matrix(resmr)), -0.1908332, tolerance = 1e-5)
-  
+
   # X and PP
   expect_error(
-    resmr <- xPPMultRight(X = X, PP = PP, Y =  matrix(rnorm(202), nrow = 101), vecchia_approx = vecchia_approx, permutate_PP_to_obs = FALSE),
+    resmr <- xPPMultRight(X = X, PP = PP, Y = matrix(rnorm(202), nrow = 101), vecchia_approx = vecchia_approx, permutate_PP_to_obs = FALSE),
     NA
   )
   expect_true(is(resmr, "Matrix"))
   expect_identical(dim(resmr), c(1000L, 2L))
   # expect_equal(mean(as.matrix(resmr)), 0.01075514, tolerance = 1e-5)
-  
 })
--- tests/testthat/test-visualisation.R
+++ tests/testthat/test-visualisation.R
@@ -1,8 +1,8 @@
 set.seed(123)
-obs_locs <- matrix(rnorm(200), ncol=2)
-vecchia_approx = createVecchia(obs_locs, ncores=1)
+obs_locs <- matrix(rnorm(200), ncol = 2)
+vecchia_approx <- createVecchia(obs_locs)
 pepito <- suppressMessages(
-  createPP(vecchia_approx, plot=FALSE)
+  createPP(vecchia_approx, plot = FALSE)
 )
 
 test_that("plot_knots.PP doesn't produce errors", {
@@ -11,7 +11,7 @@ test_that("plot_knots.PP doesn't produce errors", {
     NA
   )
   set.seed(123)
-  mar_var = rnorm(125)
+  mar_var <- rnorm(125)
   expect_error(
     res <- plot_knots.PP(pepito, mar_var_loss = mar_var),
     NA
@@ -32,10 +32,10 @@ test_that("plot.PP doesn't produce errors", {
     "0.3% of marginal variance"
   )
   expect_message(
-    res <- plot.PP(pepito, mar_var_loss = TRUE, separate=TRUE),
+    res <- plot.PP(pepito, mar_var_loss = TRUE, separate = TRUE),
     "0.3% of marginal variance"
   )
-  mar_var = var_loss_percentage.PP(pepito)
+  mar_var <- var_loss_percentage.PP(pepito)
   expect_error(
     res <- plot_knots.PP(pepito, mar_var_loss = mar_var),
     NA
@@ -45,7 +45,7 @@ test_that("plot.PP doesn't produce errors", {
     NA
   )
 })
-# 
+#
 # test_that("compare_PP_NNGP produce expected output", {
 #   obs_locs <- matrix(rnorm(20), ncol=2)
 #   ex_PP <- PP(obs_locs, matern_range=c(1, 1.1, 1.5, 0), knots=4)
@@ -57,7 +57,7 @@ test_that("plot.PP doesn't produce errors", {
 #   # Test that par is correctly reset
 #   testthat::expect_identical(op, par("mfrow"))
 # })
-# 
+#
 # test_that("plot_ellipses don't produce errors", {
 #   locs <- matrix(rnorm(20), ncol=2)
 #   log_range <- matrix(rnorm(30), ncol=3)
@@ -71,15 +71,15 @@ test_that("plot.PP doesn't produce errors", {
 #     NA
 #   )
 # })
-# 
+#
 # test_that("get_colors produce expected output", {
 #   expect_error(
-#     res<- get_colors(c(1,2,3)), 
+#     res<- get_colors(c(1,2,3)),
 #           NA
 #   )
 #   expect_identical(res, c("#FF0000", "#FF9B00", "#FFFF9E"))
 # })
-# 
+#
 # test_that("plot_pointillist_painting produce expected errors", {
 #   locs=c(2,3,6)
 #   field=c(1,2,3)
@@ -93,7 +93,7 @@ test_that("plot.PP doesn't produce errors", {
 #     NA
 #   )
 # })
-# 
+#
 # test_that("pointillist_colorscale produce expected output", {
 #   op <- par("mfrow")
 #   expect_error(
@@ -103,7 +103,7 @@ test_that("plot.PP doesn't produce errors", {
 #   # Test that par is correctly reset
 #   testthat::expect_identical(op, par("mfrow"))
 # })
-# 
+#
 # test_that("array_cumsum produce expected output", {
 #   myarray <- array(abs(rnorm(8)), dim = c(2, 2, 2))
 #   expect_error(
@@ -116,7 +116,7 @@ test_that("plot.PP doesn't produce errors", {
 #                     array(c(0.33867004,0.05762468 ,0.195550,1.094133), dim=c(2,2)))
 #   expect_equivalent(res[,,2],
 #                     array(c(0.8683650, 0.2959837, 0.2992697, 1.5791371), dim=c(2,2)))
-#   
+#
 #   myarray2 <- array(abs(rnorm(100)), dim = c(10, 10, 2))
 #   expect_error(
 #     res2 <- array_cumsum(myarray2),
@@ -124,10 +124,10 @@ test_that("plot.PP doesn't produce errors", {
 #   )
 #   expect_true(class(res2)=="array")
 #   expect_identical(dim(res2), dim(myarray2))
-#   
+#
 # })
-# 
-# 
+#
+#
 # test_that("array_multiply_3 produce expected output", {
 #   x <- array(1:12, dim = c(2, 2, 3))
 #   M <- Matrix::Matrix(matrix(1:9, nrow = 3, ncol = 3), sparse = TRUE)
@@ -140,7 +140,7 @@ test_that("plot.PP doesn't produce errors", {
 #   expect_identical(res[,,2], array(c(83,98,113,128), dim=c(2,2)))
 #   expect_identical(res[,,3], array(c(128,152,176,200), dim=c(2,2)))
 # })
-# 
+#
 # test_that("array_multiply_2 produce expected output", {
 #   x <- array(1:24, dim = c(2, 2, 4))
 #   M <- matrix(1:4, nrow = 2, ncol = 2)
@@ -150,10 +150,10 @@ test_that("plot.PP doesn't produce errors", {
 #   )
 #   # expect_identical(dim(res), dim(x))
 # })
-# 
+#
 # test_that("array_multiply_1 produce expected output", {
 #   M <- matrix(1:4, nrow = 2, ncol = 2)
-#   x <- array(1:8, dim = c(2, 2, 2)) 
+#   x <- array(1:8, dim = c(2, 2, 2))
 #   expect_error(
 #     res <- array_multiply_1(x, M),
 #     NA
@@ -162,8 +162,8 @@ test_that("plot.PP doesn't produce errors", {
 #   expect_identical(res[,,1], array(c(7, 10, 15, 22), dim=c(2,2)))
 #   expect_identical(res[,,2], array(c(23, 34, 31, 46), dim=c(2,2)))
 # })
-# 
-# 
+#
+#
 # test_that("grb_diags_field produce expected output format", {
 #   myarrays <- list(
 #     array(rnorm(100000)+10, dim = c(10, 10, 1000)),
@@ -176,30 +176,30 @@ test_that("plot.PP doesn't produce errors", {
 #   )
 #   expect_type(res, "list")
 #   expect_named(res, c("iterations", "mean", "var", "PSRF", "PSRF_quantiles"))
-#   
+#
 #   expect_true(is(res$iterations, "numeric"))
 #   expect_true(is(res$mean, "list"))
 #   expect_true(is(res$var, "list"))
 #   expect_true(is(res$PSRF, "array"))
 #   expect_true(is(res$PSRF_quantiles, "array"))
-#   
+#
 #   expect_length(res$iterations, 500)
 #   expect_length(res$mean, 3)
 #   expect_length(res$var, 3)
 #   expect_length(res$PSRF, 50000)
 #   expect_length(res$PSRF_quantiles, 2000)
-#   
+#
 #   target_dim <- c(10,10,500)
 #   expect_identical(lapply(res$mean), rep(list(target_dim)))
 #   expect_identical(lapply(res$var), rep(list(target_dim)))
 #   expect_identical(dim(res$PSRF), target_dim)
 #   expect_identical(dim(res$PSRF), target_dim)
 #   expect_identical(dim(res$PSRF_quantiles), c(4,500))
-#   
+#
 #   # Test with small data to test for output values
 # })
-# 
-# 
+#
+#
 # test_that("grb_diags_field produce expected output values", {
 #   myarrays <- list(
 #     array(rnorm(100)+10, dim = c(10, 10, 10)),
@@ -221,16 +221,16 @@ test_that("plot.PP doesn't produce errors", {
 #   res$PSRF_quantiles[is.infinite(res$PSRF_quantiles)] <- 0
 #   expect_equal(sum(res$PSRF_quantiles)/1e14, 2.8483681325, tolerance = 1e-10)
 # })
-# 
+#
 # test_that("plot_PSRF produce no error", {
 #   myarrays = list(
 #     array(rnorm(600), dim = c(4, 2, 100)),
 #     array(rnorm(600), dim = c(4, 2, 100)),
 #     array(rnorm(600), dim = c(4, 2, 100))
 #   )
-#   res = grb_diags_field(record_arrays = myarrays, 
-#                          iterations = seq(100), 
-#                          burn_in = .5, 
+#   res = grb_diags_field(record_arrays = myarrays,
+#                          iterations = seq(100),
+#                          burn_in = .5,
 #                          starting_proportion = .5)
 #   expect_error(
 #     plot_PSRF(res, varname = "Example"),
@@ -242,7 +242,7 @@ test_that("plot.PP doesn't produce errors", {
 #     NA
 #   )
 # })
-# 
+#
 # test_that("plot_log_scale produce no error", {
 #   myarrays = list(
 #       array(rnorm(6000)+ rep(c(10,10,0,10,0,0), 1000), dim = c(6, 1, 1000)),
@@ -250,8 +250,8 @@ test_that("plot.PP doesn't produce errors", {
 #       array(rnorm(6000)+ rep(c(10,10,0,10,0,0), 1000), dim = c(6, 1, 1000))
 #   )
 #   expect_error(
-#     res <- plot_log_scale(log_scale_arrays = myarrays, 
-#                         iterations = seq(100), 
+#     res <- plot_log_scale(log_scale_arrays = myarrays,
+#                         iterations = seq(100),
 #                         starting_proportion = .5,
 #                         varname = "toto"),
 #     NA
@@ -263,36 +263,36 @@ test_that("plot.PP doesn't produce errors", {
 #     array(rnorm(6000)+ rep(c(10,10,0,10,0,0), 1000), dim = c(1, 6, 1000))
 #   )
 #   expect_error(
-#     res <- plot_log_scale(log_scale_arrays = myarrays, 
-#                           iterations = seq(100), 
+#     res <- plot_log_scale(log_scale_arrays = myarrays,
+#                           iterations = seq(100),
 #                           starting_proportion = .5,
 #                           varname = "toto"),
 #     NA
 #   )
 #   expect_null(res)
 # })
-# 
+#
 # test_that("plot_beta produce no error", {
 #   beta_arrays = list(
 #     array(rnorm(400), dim = c(2, 2, 100)),
 #     array(rnorm(400), dim = c(2, 2, 100)),
 #     array(rnorm(400), dim = c(2, 2, 100))
 #   )
-#   
+#
 #   expect_error(
-#     res <- plot_beta(beta_arrays, 
-#                      iterations=seq(100), 
-#                      starting_proportion = .5, 
-#                      varname = "Example", 
+#     res <- plot_beta(beta_arrays,
+#                      iterations=seq(100),
+#                      starting_proportion = .5,
+#                      varname = "Example",
 #                      var_names = c(1, 2)),
 #     NA
 #   )
 # })
-# 
+#
 # test_that("diagnostic_plots produce no error", {
 #   # TODO
 # })
-# 
+#
 # test_that("ESS produce expected_output", {
 #   # TODO
 # })
