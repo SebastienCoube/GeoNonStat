22aa5276ff7b432b333320ced123fab900f0c6b5
Elise Maigné <elise.maigne@inrae.fr>
2026-04-13 17:18:26 +0200

    export printMcmcDiags

--- R/visualisationMCMC.R
+++ R/visualisationMCMC.R
@@ -1,5 +1,15 @@
+#' Print the summary of MCMC diagnostics
+#'
+#' @param object a GeoNonStat object
+#' @param burn_in numeric, between 0 and 1. Default to 0.1
+#' @return a list of two data.frames
+#' 
 #' @export
-printMcmcDiags <- function(object, burn_in = .1, min_ESS = 50, max_Gelman_upper = 1.05) {
+#' @keywords internal
+#' @examples
+#' MCMC_diags <- printMcmcDiags(processedGnsDemo)
+#' # TODO - quid des NA ? 
+printMcmcDiags <- function(object, burn_in = .1) {
   # only two significant digits are kept
   records <- aggregateRecords(object, burn_in = burn_in, keep = "all", keep_separate_chains = TRUE)
   ess <- lapply(records, function(x) lapply(x, function(x) coda::effectiveSize(x)))
@@ -15,7 +25,13 @@ printMcmcDiags <- function(object, burn_in = .1, min_ESS = 50, max_Gelman_upper 
   res <- t(rbind(ess, gelman_diags))
   res <- data.frame(res)
   colnames(res) <- c("ess", "Point est. Gelman", "Upper C.I. Gelman")
-
+  if(anyNA(res)) {
+    warning("Some parameters haven't been updated yet by the MCMC.",
+    "You should increase n_iterations and re-run MCMC before requesting diags.")
+    return(res)
+  }
+  
+  
   regle <- c("min", "max", "max")
   worst <- data.frame(
     criterium = colnames(res),
